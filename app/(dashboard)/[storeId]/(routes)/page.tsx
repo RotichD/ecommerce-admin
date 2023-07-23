8ec17fbf12f079513a4c0b669f2738a61becda93
4FRO <drotich@ucsc.edu>
2023-07-23 16:46:55 -0700

    - Added Navbar w/ dynamic routes - Added Searchable Popover for store switcher - Added Store update form - Added Individual Store API CRUD routes - Added Api Alert Component w/ copy url button

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,7 +1,17 @@
-import React from 'react';
+import prismadb from '@/lib/prismadb';
 
-function DashboardPage() {
-  return <div>DashboardPage</div>;
+interface DashboardPageProps {
+  params: { storeId: string };
 }
+
+const DashboardPage: React.FC<DashboardPageProps> = async ({ params }) => {
+  const store = await prismadb.store.findFirst({
+    where: {
+      id: params.storeId,
+    },
+  });
+
+  return <div>DashboardPage w/ store: {store?.name}</div>;
+};
 
 export default DashboardPage;
